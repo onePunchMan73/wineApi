93aa2ba3e94842db16c5ada69b8f2b97b10039b5
Florent Ascensio <florent.ascensio@tohero.fr>
2024-02-13 16:11:18 +0100

    add getters and setters

--- api/src/Product/Wine/Domain/Wine.php
+++ api/src/Product/Wine/Domain/Wine.php
@@ -6,9 +6,71 @@ use DateTimeInterface;
 
 class Wine
 {
+    private int $id;
     private string $name;
     private string $description;
     private DateTimeInterface $createdAt;
     private DateTimeInterface $updatedAt;
     private bool $isActive;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
 }
